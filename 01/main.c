99361cd9425f26dd9fa63eba39324aef8a3ff21e
andrazznidar <andrazznidar@users.noreply.github.com>
2019-10-22 19:32:42 +0200

    Izhodišče Vaje 01

---
+++ 01/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main ()
+{
+    // ResetBit
+    printf("Result 1: %X\n", ResetBit(0xF, 2));
+    printf("Result 2: %X\n", ResetBit(0xA, 0));
+
+    // ResetTwoBits
+    printf("Result 3: %X\n", ResetTwoBits(0xFF, 3));
+    printf("Result 4: %X\n", ResetTwoBits(0xB7, 3));
+    
+    // SetBit
+    printf("Result 5: %X\n", SetBit(0xA, 0));
+    printf("Result 6: %X\n", SetBit(0xE, 2));
+    
+    // SetTwoBitsTo
+    printf("Result 7: %X\n", SetTwoBitsTo(0xFF, 3, 1));
+    printf("Result 8: %X\n", SetTwoBitsTo(0xAF, 3, 2));  
+    
+    return 0;
+}
+
+int ResetBit(int x, int p)
+{
+}
+
+int ResetTwoBits(int x, int p)
+{
+}
+
+int SetBit(int x, int p)
+{
+}
+
+int SetTwoBitsTo(int x, int p, int n)
+{
+}
